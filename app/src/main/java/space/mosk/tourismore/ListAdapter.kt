7e8735be7f074f8f18fb9ee82a7e3e2d888f5c9a
AlexanderTereshenkov <alexandrtereshenkov@mail.ru>
2022-07-26 16:47:47 +0300

    Nice

--- app/src/main/java/space/mosk/tourismore/ListAdapter.kt
+++ app/src/main/java/space/mosk/tourismore/ListAdapter.kt
@@ -13,9 +13,8 @@ class ListAdapter(private val array: List<Way>, val onViewClick: OnViewClickList
 
     class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
         val name : TextView = itemView.findViewById(R.id.wayName)
-        val desc : TextView = itemView.findViewById(R.id.wayDescription)
         val img : ImageView = itemView.findViewById(R.id.imgCardHolder)
-
+        val littleDesc : TextView = itemView.findViewById(R.id.wayDescription)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -25,7 +24,7 @@ class ListAdapter(private val array: List<Way>, val onViewClick: OnViewClickList
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.name.text = array[position].name
-        holder.desc.text = array[position].description
+        holder.littleDesc.text = array[position].littleDescr
         holder.img.setImageResource(array[position].img)
         holder.itemView.setOnClickListener{
             onViewClick.onItemClick(position)
@@ -41,15 +40,15 @@ interface OnViewClickListener {
     fun onItemClick(pos : Int)
 }
 
-data class Way(val name :String, val description : String, val img : Int, val srcImg : Int)
+data class Way(val name :String,val littleDescr: String, val description : String, val img : Int, val srcImg : Int)
 
 fun makeSampleWays() : List<Way>{
     return listOf(
-        Way("Кремль", "Годнота", R.drawable.squared_img, R.drawable.kremlin),
-        Way("Также кремль", "Годнота", R.drawable.squared_img, R.drawable.kremlin),
-        Way("Еще один", "Годнота", R.drawable.squared_img, R.drawable.kremlin),
-        Way("Почти", "Супер пупер очень круто", R.drawable.squared_img, R.drawable.kremlin),
-        Way("ААААААА", "ааааааааааааааа", R.drawable.squared_img, R.drawable.kremlin),
-        Way("ббббббббббб", "ббббббббббббб", R.drawable.squared_img, R.drawable.kremlin),
+        Way("Кремль", "15 KM","Годнота", R.drawable.squared_img, R.drawable.kremlin),
+        Way("Также кремль", "25 KM","Годнота", R.drawable.squared_img, R.drawable.kremlin),
+        Way("Еще один", "16 KM","Годнота", R.drawable.squared_img, R.drawable.kremlin),
+        Way("Почти", "12 KM","Супер пупер очень круто", R.drawable.squared_img, R.drawable.kremlin),
+        Way("ААААААА", "1225 KM","ааааааааааааааа", R.drawable.squared_img, R.drawable.kremlin),
+        Way("ббббббббббб", "24 KM","ббббббббббббб", R.drawable.squared_img, R.drawable.kremlin),
     )
 }
