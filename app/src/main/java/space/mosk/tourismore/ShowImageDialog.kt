4e2305849040827f0fbc098649e3257b29c2e851
AlexanderTereshenkov <alexandrtereshenkov@mail.ru>
2022-07-28 16:25:54 +0300

    open dialog photo

--- app/src/main/java/space/mosk/tourismore/ShowImageDialog.kt
+++ app/src/main/java/space/mosk/tourismore/ShowImageDialog.kt
@@ -3,15 +3,9 @@ package space.mosk.tourismore
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.bumptech.glide.Glide
-import org.w3c.dom.Text
 
 class ShowImageDialog(val url : String): DialogFragment() {
     private lateinit var img : ImageView
@@ -19,7 +13,6 @@ class ShowImageDialog(val url : String): DialogFragment() {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
             val inflater = requireActivity().layoutInflater;
-
             val dialogView = inflater.inflate(R.layout.dialog_image_layout, null)
             builder.setView(dialogView)
             img = dialogView.findViewById(R.id.showImg)
@@ -27,5 +20,4 @@ class ShowImageDialog(val url : String): DialogFragment() {
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
-
 }
